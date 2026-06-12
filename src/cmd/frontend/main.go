a50f365415d561d13e8a973a5d079d3e7bdfae2d
WillMorrison <william.barr.morrison@gmail.com>
2026-06-12 08:33:08 +0200

    feat: Player Actions in frontend

--- src/cmd/frontend/main.go
+++ src/cmd/frontend/main.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"strconv"
 
 	"github.com/WillMorrison/JouleQuestCardGame/assets"
 	"github.com/WillMorrison/JouleQuestCardGame/core"
@@ -32,7 +33,7 @@ type playerInfo struct {
 	Actions  []playerAction
 }
 
-type page struct {
+type gameInfo struct {
 	Status                   core.GameStatus
 	Reason                   core.LossCondition
 	Round                    int
@@ -42,6 +43,7 @@ type page struct {
 	LastRoundPriceVolatility core.PriceVolatility
 	TakeoverPool             assets.AssetMix
 	Players                  []playerInfo
+	Params                   params.Params
 }
 
 //go:embed assets/game.html.tmpl
@@ -52,7 +54,7 @@ var pageTemplate = template.Must(template.New("page").Parse(string(templateSrc))
 func renderGame(g engine.ProceduralGameState) ([]byte, error) {
 	actions := g.PossibleActions()
 	gs := g.Game()
-	tmplInput := page{
+	tmplInput := gameInfo{
 		Status:                   gs.Status,
 		Reason:                   gs.Reason,
 		Round:                    gs.Round,
@@ -61,6 +63,7 @@ func renderGame(g engine.ProceduralGameState) ([]byte, error) {
 		LastRoundPriceVolatility: gs.LastSnapshot.PriceVolatility,
 		LastRoundAssetMix:        gs.LastSnapshot.AssetMix,
 		TakeoverPool:             gs.TakeoverPool,
+		Params:                   gs.Params,
 	}
 	for pi, p := range gs.Players {
 		info := playerInfo{
@@ -77,9 +80,6 @@ func renderGame(g engine.ProceduralGameState) ([]byte, error) {
 		}
 		tmplInput.Players = append(tmplInput.Players, info)
 	}
-	log.Printf("Game: %+v", gs)
-	log.Printf("Actions: %+v", actions)
-	log.Printf("Template Input: %+v", tmplInput)
 	var b bytes.Buffer
 	if err := pageTemplate.Execute(&b, tmplInput); err != nil {
 		return nil, err
@@ -87,18 +87,85 @@ func renderGame(g engine.ProceduralGameState) ([]byte, error) {
 	return b.Bytes(), nil
 }
 
-func GameRenderHandler(g *engine.ProceduralGameState) http.HandlerFunc {
-	return func(resp http.ResponseWriter, req *http.Request) {
-		resp.Header().Set("Content-Type", "text/html")
-		b, err := renderGame(*g)
+type handler struct {
+	g      engine.ProceduralGameState
+	logger eventlog.Logger
+}
+
+func (h handler) Mux() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /{$}", h.render)
+	mux.HandleFunc("POST /{$}", h.applyAction)
+	mux.HandleFunc("/new", h.newGame())
+	return mux
+}
+
+func (h handler) render(resp http.ResponseWriter, req *http.Request) {
+	resp.Header().Set("Content-Type", "text/html")
+	b, err := renderGame(h.g)
+	if err != nil {
+		resp.WriteHeader(http.StatusInternalServerError)
+		resp.Write([]byte(err.Error()))
+		log.Printf("Render error: %v", err)
+		return
+	}
+	resp.Write(b)
+}
+
+func (h *handler) applyAction(resp http.ResponseWriter, req *http.Request) {
+	req.ParseForm()
+	actionIndex := req.Form.Get("action")
+	if actionIndex == "" {
+		resp.WriteHeader(http.StatusBadRequest)
+		log.Printf("No action provided")
+	} else {
+		ai, err := strconv.Atoi(actionIndex)
 		if err != nil {
-			resp.WriteHeader(http.StatusInternalServerError)
-			resp.Write([]byte(err.Error()))
-			log.Printf("Render error: %v", err)
-			return
+			resp.WriteHeader(http.StatusBadRequest)
+			log.Printf("Invalid action index: %v", err)
+		} else {
+			actions := h.g.PossibleActions()
+			if ai < 0 || ai >= len(actions) {
+				resp.WriteHeader(http.StatusBadRequest)
+				log.Printf("Action index out of range: %d", ai)
+			} else {
+				h.g.ApplyPlayerAction(actions[ai])
+			}
 		}
-		resp.WriteHeader(http.StatusOK)
-		resp.Write(b)
+	}
+	h.render(resp, req)
+}
+
+func (h *handler) newGame() http.HandlerFunc {
+	return func(resp http.ResponseWriter, req *http.Request) {
+		gameParams := params.Default
+		numPlayers := 4
+
+		req.ParseForm()
+		numPlayersStr := req.Form.Get("players")
+		if numPlayersStr != "" {
+			n, err := strconv.Atoi(numPlayersStr)
+			if err != nil {
+				resp.WriteHeader(http.StatusBadRequest)
+				resp.Write([]byte("Invalid number of players"))
+				return
+			}
+			if _, ok := gameParams.StartingFossilAssetsPerPlayer[n]; !ok {
+				resp.WriteHeader(http.StatusBadRequest)
+				resp.Write([]byte("Unsupported number of players"))
+				return
+			}
+			numPlayers = n
+		}
+
+		g, err := engine.NewProceduralGame(numPlayers, gameParams, h.logger)
+		if err != nil {
+			log.Fatalf("Unable to create Game: %v", err)
+		}
+		h.g = *g
+		log.Print("Started a new game")
+
+		http.Redirect(resp, req, "/", http.StatusSeeOther)
 	}
 }
 
@@ -109,22 +176,26 @@ func (_ logWriter) Write(p []byte) (n int, err error) {
 	return len(p), nil
 }
 
-func main() {
-	var netAddr string
-	flag.StringVar(&netAddr, "addr", "localhost:0", "Address in host:port format. If the port is 0 or empty, an unused port will be selected.")
-	flag.Parse()
-
+func NewHandler() handler {
 	eventLogger := eventlog.NewJsonLogger(logWriter{})
 	g, err := engine.NewProceduralGame(4, params.Default, eventLogger)
 	if err != nil {
 		log.Fatalf("Unable to create Game: %v", err)
 	}
-	http.HandleFunc("/game", GameRenderHandler(g))
+	return handler{g: *g, logger: eventLogger}
+}
+
+func main() {
+	var netAddr string
+	flag.StringVar(&netAddr, "addr", "localhost:0", "Address in host:port format. If the port is 0 or empty, an unused port will be selected.")
+	flag.Parse()
+
+	h := NewHandler()
 	listener, err := net.Listen("tcp", netAddr)
 	if err != nil {
 		log.Fatalf("Unable to start TCP listener: %v", err)
 	}
 	log.Printf("Listening on %s", listener.Addr().String())
-	server := http.Server{}
+	server := http.Server{Handler: h.Mux()}
 	server.Serve(listener)
 }
